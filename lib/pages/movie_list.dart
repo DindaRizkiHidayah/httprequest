84c04408f62782106868a017c301bde4c16008b5
DindaRizkiHidayah <70737564+DindaRizkiHidayah@users.noreply.github.com>
2021-04-12 11:18:35 +0700

    Add files via upload

--- lib/pages/movie_list.dart
+++ lib/pages/movie_list.dart
@@ -42,6 +42,8 @@ class _MovieListState extends State<MovieList> {
               color: Colors.white,
               elevation: 2.0,
               child: ListTile(
+                leading: Image.network('https://www.themoviedb.org/t/p/w600_and_h900_bestv2/'+
+                movies[position].posterPath),
                 title: Text(movies[position].originalTitle),
                 subtitle:
                     Text('Rating = ' + movies[position].voteAverage.toString()),
